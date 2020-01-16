4b83ff973437bde14a5d978403d311d6d9acbc02
mrunknown.892@gmail.com <mrunknown.892@gmail.com>
2020-01-16 11:45:05 -0800

    updated the api for authentication'

--- announcements/index.js
+++ announcements/index.js
@@ -1,14 +1,54 @@
+const { authenticateApiKey, authenticatePassword, parseRequestBody } = require("./middleware");
 const { sendAnnouncement } = require("./twilio");
 
 module.exports = async function(context, req) {
-    return sendAnnouncement(context, req.body.message)
-        .then(() => {
+
+    const isAuthenticated = authenticateApiKey(context, req);
+    const passwordAuthentication = authenticatePassword(context, req);
+    const parseAnnouncement = parseAnnouncement(context, req);
+
+    if (!isAuthenticated) {
+        return {
+            status: 401,
+            body: {
+                error: true,
+                status: 401,
+                message: "Unable to authenticate request",       
+            },
+        };
+    };
+
+    if (!passwordAuthentication) {
+        return {
+            status: 403,
+            body: {
+                error: true,
+                status: 403,
+                message: "Incorrect API password",
+            }
+        }
+    }
+
+    if (!parseRequestBody) {
+        return {
+            status: 400,
+            body: {
+                error: true,
+                status: 400,
+                message: "Incorrect request body.",
+            }
+        }
+    }
+
+    if (req.body.twilio) {
+        return sendAnnouncement(context, req.body.announcement)
+        .then((val) => {
             return {
                 status: 200,
                 body: {
                     error: false,
                     status: 200,
-                    message: 'message sent successfully',
+                    message: `Messages sent succesfully: ${val.sentNumberCount}, Messages not sent succesfully: ${val.unsentNumberCount}`,
                 },
             };
         })
@@ -22,4 +62,8 @@ module.exports = async function(context, req) {
                 },
             };
         });
+    }
+    else {
+        
+    }
 };
--- announcements/twilio.js
+++ announcements/twilio.js
@@ -2,99 +2,50 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host: `localhost`,
-      user: `joseph`,
-      password: `joseph`,
-      database: `cruzhacks-2020`
+      host: `${process.env.DATABASE_HOST}`,
+      user: `${process.env.DATABASE_USER}`,
+      password: `${process.env.DATABASE_PASSWORD}`,
+      database: `${process.env.DATABASE_NAME}`
     }
 });
 
 const twilioConfig = {
-    accSid: `AC2a169cef76f51004f1b67066172a746f`,
-    authTok: `3567e9d9790528b42252cd3e642a33cd`,
-    phoneNum: `+16193892265`,
-};
+    accSid: `${process.env.TWILIO_ACCOUNT_SID}`,
+    authTok: `${process.env.TWILIO_AUTH_TOKEN}`,
+    phoneNum: `${process.env.TWILIO_PHONE_NUMBER}`,
+}
 
 // initializing twilio client
 const twilioClient = require('twilio')(twilioConfig.accSid, twilioConfig.authTok);
 
-const sendMessage = (message, hackerPhoneNum) => {
+const sendMessageToTwilio = (message, hackerPhoneNum) => {
 
-    const result = await twilioClient.messages.create({
+    const result = twilioClient.messages.create({
         from: twilioConfig.phoneNum,
         to: hackerPhoneNum,
         body: message
     });
-
     return result;
-
 }
 
 const sendAnnouncement = async (functionContext, message) => {
 
     const phoneNumbersObj = await knex.select('phoneNumber').from('hackers_test');
     
-    /*
-    // Batching promises together (test)
-    const promises = [];
-    phoneNumbersObj.forEach(v => {
-        promises.push(twilioClient.messages.create({from: twilioConfig.phoneNum, to: `${v["phoneNumber"]}`, body: message}))
-    });
-    Promise.all(promises).then(val => successful.push(val));
-    console.log("line3444");
-    console.log(successful);
-    */
-    /*
-    const result = await Promise.all(phoneNumbersObj.map(v => {
-        twilioClient.messages.create({from: twilioConfig.phoneNum, to: `${v["phoneNumber"]}`, body: message})
-    }));
-
-    console.log(result);
-    */
+    let totalNumber = phoneNumbersObj.length;
+    let unsentNumberCount = 0;
 
     for (const phoneNumber of phoneNumbersObj) {
-
-        const result = await 
-
-
+        try {
+            await sendMessageToTwilio(message, phoneNumber.phoneNumber);
+        }
+        catch (ex) {
+            unsentNumberCount++;
+            console.log(`Error for number: ${phoneNumber.phoneNumber} Reason:${ex}`)
+        }
     }
-        /*
-        twilioClient.messages.create({
-            from: twilioConfig.phoneNum,
-            to: `${phoneNumbersObj[i]["phoneNumber"]}`,
-            body: message
-        }).then(message => {
-            successful.push({ "phoneNumber": v["phoneNumber"] });
-            console.log(`success: ${successful}`);
-            // functionContext.log(JSON.stringify(response.data, null, 2));
-            // return Promise.resolve(true);
-        }).catch(error => {
-            errors.push({ "phoneNumber": v["phoneNumber"] });
-            console.log(`error: ${errors}`);
-            //functionContext.log.error(error.response.data.detail);
-            // return Promise.reject(new Error(`Twilio Error`));
-        });
-        */
-    }
-   /*
-    phoneNumbersObj.forEach(async v => {
-        twilioClient.messages.create({
-            from: twilioConfig.phoneNum,
-            to: `${v["phoneNumber"]}`,
-            body: message
-        }).then(message => {
-            successful.push({ "phoneNumber": v["phoneNumber"] });
-            console.log(successful);
-            // functionContext.log(JSON.stringify(response.data, null, 2));
-            // return Promise.resolve(true);
-        }).catch(error => {
-            errors.push({ "phoneNumber": v["phoneNumber"] });
-            console.log(errors);
-            //functionContext.log.error(error.response.data.detail);
-            // return Promise.reject(new Error(`Twilio Error`));
-        });
-    });
-    */
+
+    return { sentNumberCount: totalNumber - unsentNumberCount, unsentNumberCount: unsentNumberCount };
 }
 
 module.exports = {
