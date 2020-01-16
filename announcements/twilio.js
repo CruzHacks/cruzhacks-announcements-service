f0afbe0ee6afdab39d31c10dc5b281d575bcb74e
mrunknown.892@gmail.com <mrunknown.892@gmail.com>
2020-01-15 17:22:23 -0800

    updating twilio endpoint

--- announcements/twilio.js
+++ announcements/twilio.js
@@ -2,40 +2,99 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host: `${process.env.DB_HOST}`,
-      user: `${process.env.DB_USER}`,
-      password: `${process.env.DB_PASSWORD}`,
-      database: `${process.env.DB_NAME}`
+      host: `localhost`,
+      user: `joseph`,
+      password: `joseph`,
+      database: `cruzhacks-2020`
     }
 });
 
 const twilioConfig = {
-    accSid: `${process.env.TWILIO_ACC_SID}`,
-    authTok: `${process.env.TWILIO_AUTH_TOKEN}`,
-    phoneNum: `${process.env.TWILIO_PHONE_NUMBER}`,
+    accSid: `AC2a169cef76f51004f1b67066172a746f`,
+    authTok: `3567e9d9790528b42252cd3e642a33cd`,
+    phoneNum: `+16193892265`,
 };
 
 // initializing twilio client
 const twilioClient = require('twilio')(twilioConfig.accSid, twilioConfig.authTok);
 
+const sendMessage = (message, hackerPhoneNum) => {
+
+    const result = await twilioClient.messages.create({
+        from: twilioConfig.phoneNum,
+        to: hackerPhoneNum,
+        body: message
+    });
+
+    return result;
+
+}
+
 const sendAnnouncement = async (functionContext, message) => {
 
     const phoneNumbersObj = await knex.select('phoneNumber').from('hackers_test');
-
+    
+    /*
+    // Batching promises together (test)
+    const promises = [];
     phoneNumbersObj.forEach(v => {
-        // console.log(v["phoneNumber"]);
+        promises.push(twilioClient.messages.create({from: twilioConfig.phoneNum, to: `${v["phoneNumber"]}`, body: message}))
+    });
+    Promise.all(promises).then(val => successful.push(val));
+    console.log("line3444");
+    console.log(successful);
+    */
+    /*
+    const result = await Promise.all(phoneNumbersObj.map(v => {
+        twilioClient.messages.create({from: twilioConfig.phoneNum, to: `${v["phoneNumber"]}`, body: message})
+    }));
+
+    console.log(result);
+    */
+
+    for (const phoneNumber of phoneNumbersObj) {
+
+        const result = await 
+
+
+    }
+        /*
+        twilioClient.messages.create({
+            from: twilioConfig.phoneNum,
+            to: `${phoneNumbersObj[i]["phoneNumber"]}`,
+            body: message
+        }).then(message => {
+            successful.push({ "phoneNumber": v["phoneNumber"] });
+            console.log(`success: ${successful}`);
+            // functionContext.log(JSON.stringify(response.data, null, 2));
+            // return Promise.resolve(true);
+        }).catch(error => {
+            errors.push({ "phoneNumber": v["phoneNumber"] });
+            console.log(`error: ${errors}`);
+            //functionContext.log.error(error.response.data.detail);
+            // return Promise.reject(new Error(`Twilio Error`));
+        });
+        */
+    }
+   /*
+    phoneNumbersObj.forEach(async v => {
         twilioClient.messages.create({
             from: twilioConfig.phoneNum,
             to: `${v["phoneNumber"]}`,
             body: message
-        }).then(response => {
-            console.log(response);
-            return Promise.resolve(true);
-        }).catch(err => {
-            functionContext.log.error(error.response.data.detail);
-            return Promise.reject(new Error(`Twilio Error`));
+        }).then(message => {
+            successful.push({ "phoneNumber": v["phoneNumber"] });
+            console.log(successful);
+            // functionContext.log(JSON.stringify(response.data, null, 2));
+            // return Promise.resolve(true);
+        }).catch(error => {
+            errors.push({ "phoneNumber": v["phoneNumber"] });
+            console.log(errors);
+            //functionContext.log.error(error.response.data.detail);
+            // return Promise.reject(new Error(`Twilio Error`));
         });
     });
+    */
 }
 
 module.exports = {
