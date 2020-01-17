0be8f26a1c6818e0336b4d3fdab8a6438da5f25c
mrunknown.892@gmail.com <mrunknown.892@gmail.com>
2020-01-17 00:19:37 -0800

    eventbrite

---
+++ announcements/eventbrite.js
@@ -0,0 +1,80 @@
+// database initialization
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host: `${process.env.DATABASE_HOST}`,
+      user: `${process.env.DATABASE_USER}`,
+      password: `${process.env.DATABASE_PASSWORD}`,
+      database: `${process.env.DATABASE_NAME}`
+    }
+});
+
+const axios = require('axios');
+
+const eventBriteConfiguration = {
+    endpoint: `${process.env.EVENTBRITE_ATTENDEE_ENDPOINT}`
+}
+
+const requestConfiguration = {
+    headers: 
+    { 
+        "Authorization": `Bearer ${process.env.EVENTBRITE_BEARER_TOK}`
+    }
+}
+
+const getAllAttendees = async (functionContext) => {
+    
+    let numOfPages = 0;
+    let pageSize = 0;
+
+    let results;
+    
+    try {
+        results = await axios.get(eventBriteConfiguration.endpoint, { headers: { Authorization: `Bearer ${process.env.EVENTBRITE_BEARER_TOK}` }});
+    }
+    catch (ex) {
+        console.log(ex);
+    }
+
+    numOfPages = results.data.pagination.page_count;
+    pageSize = results.data.pagination.page_size;
+
+
+    let attendeesProfile = [];
+
+    
+    for (let i = 0; i < pageSize; i++) {
+        attendeesProfile.push(results.data.attendees[i].profile);
+    }
+
+    // console.log(attendeesProfile);
+
+    for (let i = 1; i < numOfPages; i++) {
+
+        // last continuation page doesnt contain continuation url
+        const continuationURL = results.data.pagination.continuation;
+
+        try {
+            results = await axios.get(`${eventBriteConfiguration.endpoint}?continuation=${continuationURL}`, { headers: { Authorization: `Bearer ${process.env.EVENTBRITE_BEARER_TOK}` }});
+        }
+        catch (ex) {
+            console.log(ex);
+        }
+
+        pageSize = results.data.pagination.page_size;
+
+        // console.log(pageSize);
+        
+        for (let i = 0 ; i < pageSize; i++) {
+            if (results.data.attendees[i].profile !== undefined) {
+                attendeesProfile.push(results.data.attendees[i].profile);
+            }
+        }
+    
+    }
+
+}
+
+module.exports = { 
+    getAllAttendees,
+}
--- announcements/index.js
+++ announcements/index.js
@@ -1,11 +1,12 @@
 const { authenticateApiKey, authenticatePassword, parseRequestBody } = require("./middleware");
+const { getAllAttendees } = require("./eventbrite");
 const { sendAnnouncement } = require("./twilio");
 
 module.exports = async function(context, req) {
 
     const isAuthenticated = authenticateApiKey(context, req);
-    const passwordAuthentication = authenticatePassword(context, req);
-    const parseAnnouncement = parseAnnouncement(context, req);
+
+    getAllAttendees(context);
 
     if (!isAuthenticated) {
         return {
@@ -18,52 +19,61 @@ module.exports = async function(context, req) {
         };
     };
 
-    if (!passwordAuthentication) {
-        return {
-            status: 403,
-            body: {
-                error: true,
+    if (req.method === "GET") {
+
+    }
+    else if (req.method === "POST") {
+
+        //const passwordAuthentication = authenticatePassword(context, req);
+        // const parseRequestBody = parseRequestBody(context, req);
+        /*
+        if (!passwordAuthentication) {
+            return {
                 status: 403,
-                message: "Incorrect API password",
-            }
-        }
-    }
-
-    if (!parseRequestBody) {
-        return {
-            status: 400,
-            body: {
-                error: true,
-                status: 400,
-                message: "Incorrect request body.",
-            }
-        }
-    }
-
-    if (req.body.twilio) {
-        return sendAnnouncement(context, req.body.announcement)
-        .then((val) => {
-            return {
-                status: 200,
-                body: {
-                    error: false,
-                    status: 200,
-                    message: `Messages sent succesfully: ${val.sentNumberCount}, Messages not sent succesfully: ${val.unsentNumberCount}`,
-                },
-            };
-        })
-        .catch(err => {
-            return {
-                status: 500,
                 body: {
                     error: true,
-                    status: 500,
-                    message: err.message,
-                },
-            };
-        });
-    }
-    else {
+                    status: 403,
+                    message: "Incorrect API password",
+                }
+            }
+        }
+        */
+        /*
+        if (!parseRequestBody) {
+            return {
+                status: 400,
+                body: {
+                    error: true,
+                    status: 400,
+                    message: "Incorrect request body.",
+                }
+            }
+        }
+        */
         
+        // 
+        if (req.body.twilio) {
+            return sendAnnouncement(context, req.body.announcement)
+            .then((val) => {
+                return {
+                    status: 200,
+                    body: {
+                        error: false,
+                        status: 200,
+                        message: `Messages sent succesfully: ${val.sentNumberCount}, Messages not sent succesfully: ${val.unsentNumberCount}`,
+                    },
+                };
+            })
+            .catch(err => {
+                return {
+                    status: 500,
+                    body: {
+                        error: true,
+                        status: 500,
+                        message: err.message,
+                    },
+                };
+            });
+        }
     }
 };
