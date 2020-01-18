e6b3e6ffd52f1f42e3985ca57c04f680ca10c37c
Isai Lopez <ilopezro@ucsc.edu>
2020-01-17 22:30:06 -0800

    added correct api key

--- announcements/middleware.js
+++ announcements/middleware.js
@@ -1,12 +1,10 @@
 const authenticateApiKey = (functionContext, requestObject ) => {
     const { headers } = requestObject;
-    const correctKey = `${process.env.API_KEY}`;
-    // const correctKey = process.env.API_KEY;
+    const correctKey = `${process.env.REACT_APP_API_KEY}`;
     if (correctKey === undefined) {
         functionContext.log.error("ERROR: UNSET API KEY ENV VAR");
         return false;
     }
-
     return headers.authentication === correctKey;
 }
 
